a4086a13ed08084f37dd1bfe1b8fcf60644be4c3
Andrei <andrsidorchenko@gmail.com>
2023-12-30 02:10:04 +0300

    Add gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import gcd from '../src/games/gcd.js';
+
+console.log('Welcome to the Brain Games!');
+gcd();
---
+++ src/games/gcd.js
@@ -0,0 +1,42 @@
+import {
+  userName,
+  askQuestion,
+  compare,
+  getRandomInt,
+} from '../index.js';
+
+function getCorrectAnswer(num1, num2) {
+  let a = num1;
+  let b = num2;
+  if (num1 > num2) {
+    while (b !== 0) {
+      const temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
+  }
+  while (a !== 0) {
+    const temp = a;
+    a = b % a;
+    b = temp;
+  }
+  return b;
+}
+
+export default function gcd() {
+  const name = userName();
+  console.log('Find the greatest common divisor of given numbers.');
+  for (let i = 0; i < 3; i += 1) {
+    const num1 = getRandomInt(1, 100);
+    const num2 = getRandomInt(1, 100);
+    const correctAnswer = getCorrectAnswer(num1, num2);
+    const userAnswer = Number(askQuestion(`${num1} ${num2}`));
+    const comparison = compare(userAnswer, correctAnswer);
+    if (comparison === 'lose') {
+      return console.log(`Let's try again, ${name}!`);
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+  return 'win';
+}
