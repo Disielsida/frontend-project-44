3310893d4f8e4d3d4f239102a0800ec5b28023cb
Andrei <andrsidorchenko@gmail.com>
2024-01-04 19:03:36 +0300

    A

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
-import userName from '../src/cli.js';
+import readlineSync from 'readline-sync';
 
-userName();
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have you name?: ');
+console.log(`Hello, ${name}!`);
