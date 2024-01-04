177df7945a117f146c1e171d098f6002d1dcba76
Andrei <andrsidorchenko@gmail.com>
2024-01-05 00:54:54 +0300

    fix all

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,22 +1,25 @@
 import { prepareGame } from '../index.js';
 
+const isPrime = (number) => {
+  if (number <= 1) {
+    return false;
+  }
+  if (number === 2) {
+    return true;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const primeGameLogic = (number) => (isPrime(number) ? 'yes' : 'no');
+
 const prime = () => {
   const questionText = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const isPrime = (number) => {
-    if (number <= 1) {
-      return 'no';
-    }
-    if (number === 2) {
-      return 'yes';
-    }
-    for (let i = 2; i <= Math.sqrt(number); i += 1) {
-      if (number % i === 0) {
-        return 'no';
-      }
-    }
-    return 'yes';
-  };
-  prepareGame(questionText, isPrime);
+  prepareGame(questionText, primeGameLogic);
 };
 
 export default prime;
